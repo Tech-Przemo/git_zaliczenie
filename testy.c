9bebd09ce65034207c706a882583deb83baac719
Przemek Okreglicki <przemok2001@gmail.com>
2022-05-14 14:05:36 +0200

    Testy CopyString zakonczone powodzeniem -> pierwszy string dluzszy, drugi krotszy, odwrotna sytuacja, oraz stringi tej samej dlugosci

--- testy.c
+++ testy.c
@@ -8,7 +8,7 @@ void TestOf_CopyString(void)
     printf("CopyString \n\n");
 
     printf("Test 1 - ");
-    //Czy funkcja dziala 
+    //Czy funkcja dziala (pierwszy dluzszy, drugi krotszy)
     char cAfterCopy_1[] = "test1";
     CopyString("abcd", cAfterCopy_1);
     
@@ -18,6 +18,24 @@ void TestOf_CopyString(void)
         printf("ERROR\n");
 
     printf("Test 2 - ");
+    //Pierwszy lancuch dluzszy drugi krotszy
+    char cAfterCopy_2[] = "test2";
+    CopyString("ABCDEFG", cAfterCopy_2);
+
+    if(strcmp("ABCDEFG", cAfterCopy_2) == 0)
+        printf("OK\n");
+    else
+        printf("ERROR\n");
+
+    printf("Test 3 - ");
+    //Lancuchy tej samej dlugosci 
+    char cAfterCopy_3[] = "test3";
+    CopyString("ABCDE", cAfterCopy_3);
+
+    if(strcmp("ABCDE", cAfterCopy_3) == 0)
+        printf("OK\n");
+    else
+        printf("ERROR\n");
 }
 
 
